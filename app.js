1e8190df6cfbf403ae302018cb842334f59c978f
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-07-05 22:08:46 +0530

    small changes

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('.dice').style.display = 'none';
+document.querySelector(".dice").style.display = "none";
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    // Random nnumber
-    var dice = Math.floor(Math.random() * 6) + 1;
-    
-    // Display the result.
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.getElementsByClassName.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // Random nnumber
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // Display the result.
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.getElementsByClassName.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
 });
